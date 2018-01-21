4dcb49c5d452ae6bc509ac11f4fb1aafcd5f760f
amitrke <amitrke@gmail.com>
2018-01-21 16:30:33 -0500

    Moved from Node to Spring boot.

---
+++ src/main/java/org/roorkee/rkerestapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.roorkee.rkerestapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RkerestapiApplication.class);
+	}
+
+}
