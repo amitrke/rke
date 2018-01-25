27584b1d7833a63fbaebe42ba84e347816247199
amitrke <amitrke@gmail.com>
2018-01-25 15:59:53 -0500

    Appengine datastore create entity

--- src/main/java/org/roorkee/rkerestapi/ContentController.java
+++ src/main/java/org/roorkee/rkerestapi/ContentController.java
@@ -1,6 +1,11 @@
 package org.roorkee.rkerestapi;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,15 @@ public class ContentController {
     @GetMapping("/hello")
     public String hello() {
         return "hello world!";
+    }
+
+    @PostMapping("/testCreate")
+    public String testCreate() {
+        Entity testEntity = new Entity("TestEntity");
+        testEntity.setProperty("name", "Test Name");
+        testEntity.setProperty("value", "Test Value");
+        DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+        Key k = datastoreService.put(testEntity);
+        return k.toString();
     }
 }
