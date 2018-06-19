839abac7281234d257d447e5d1199d15f6ac37bb
amitrke <amitrke@gmail.com>
2018-06-18 22:40:58 -0400

    Abstract DAO class

--- src/main/java/org/roorkee/rkerestapi/controller/AbstractBaseController.java
+++ src/main/java/org/roorkee/rkerestapi/controller/AbstractBaseController.java
@@ -1,6 +1,17 @@
 package org.roorkee.rkerestapi.controller;
 
-public class AbstractBaseController {
+import org.roorkee.rkerestapi.entity.AbstractEntity;
 
+import java.util.List;
+
+public abstract class AbstractBaseController<T extends AbstractEntity> {
+
+    public abstract T save(T entity);
+
+    public abstract List<T> list();
+
+    public abstract T get(Long id);
+
+    public abstract void delete(Long id);
 
 }
--- src/test/java/org/roorkee/rkerestapi/controller/ArticleControllerTest.java
+++ src/test/java/org/roorkee/rkerestapi/controller/ArticleControllerTest.java
@@ -2,4 +2,8 @@ package org.roorkee.rkerestapi.controller;
 
 public class ArticleControllerTest extends AbstractBaseControllerTest {
 
+    @Override
+    public void test_post() {
+
+    }
 }
