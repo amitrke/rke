c42731fb4b26fbf24cb5c7eb6b144aad9d4f8e30
Amit Kumar <amitrke@gmail.com>
2018-07-23 08:45:40 -0400

    Added Swagger documentation

--- src/main/java/org/roorkee/rkerestapi/dao/UserDao.java
+++ src/main/java/org/roorkee/rkerestapi/dao/UserDao.java
@@ -1,6 +1,5 @@
 package org.roorkee.rkerestapi.dao;
 
-import com.google.appengine.api.datastore.Key;
 import org.roorkee.rkerestapi.entity.User;
 import org.springframework.stereotype.Repository;
 
