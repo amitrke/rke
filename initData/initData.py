a56a2bce55693de98e63ef02c83f1e9f472325ee
Amit Kumar <amitrke@gmail.com>
2018-09-06 18:46:52 -0400

    Added init script to upload image file

--- initData/initData.py
+++ initData/initData.py
@@ -9,11 +9,11 @@ def restCall(dataBlock):
     if (response.status_code != 200):
         print(response.status_code)
     else:
-        print(response.json())
+        print(response.text)
     
     return response
 
-
+# Create user
 userObj = {
         "url": "users/",
         "type": "POST",
@@ -27,16 +27,24 @@ if resp.status_code != 200:
 
 userId = resp.json()["response"]
 
+# Upload an image
+img = open('requests/iitr.jpg', 'rb')
+files = {'file':img}
+headers = {'filename':'/users/'+userId+'/iitr.jpg'}
+r = requests.post('http://localhost:8080/api/image/', files=files, headers=headers)
+print r.text
+
+
 dataJSON = [
     {
         "url": "articles/",
         "type": "POST",
-        "data": {"userId":userId,"status":"Active","title":"Test Title","imageURL":"/abc/def/a.jpg","description":"Test Description","fullText":"Test Full Text","priority":1}
+        "data": {"userId":userId,"status":"Active","title":"Test Title","imageURL":'/users/'+userId+'/iitr.jpg',"description":"Test Description","fullText":"Test Full Text","priority":1}
     },
     {
         "url": "articles/",
         "type": "POST",
-        "data": {"userId":userId,"status":"Active","title":"Test Title","imageURL":"/abc/def/a.jpg","description":"Test Description","fullText":"Test Full Text","priority":1}
+        "data": {"userId":userId,"status":"Active","title":"Test Title","imageURL":'/users/'+userId+'/iitr.jpg',"description":"Test Description","fullText":"Test Full Text","priority":1}
     }
 ]
 
